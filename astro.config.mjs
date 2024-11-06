a3b7de498c02d612ec22bd5733ae718597d17d90
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2024-11-06 12:30:32 +0900

    SSRアダプターをアンインストール

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 import svelte from '@astrojs/svelte';
 
-import vercel from '@astrojs/vercel/serverless';
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://kensuke-ota.vercel.app",
   integrations: [tailwind(), svelte()],
-  output: 'server',
-  adapter: vercel()
 });
