635cc99b9a7b04f071338421c99dfaefda462e8b
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2024-11-06 09:59:05 +0900

    RSSフィードを作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kensuke-ota.vercel.app",
   integrations: [tailwind(), svelte()]
 });
